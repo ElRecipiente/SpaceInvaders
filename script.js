a92384e5c018c186ec63e8d8247c610471b4dff2
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-02-15 11:56:31 +0100

    space invaders V1 + sprites

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 let theShip = document.getElementById("ship");
 let theMain = document.querySelector("main");
+let showScore = document.querySelector("#score span")
+let score = 0;
 let theMissile = document.getElementById("missile");
 let invadersLine1 = document.querySelector("div.invadersLine1");
 let invadersLine2 = document.querySelector("div.invadersLine2");
 let invadersLine3 = document.querySelector("div.invadersLine3");
-let shipX = 50;
-let missileToShip = 97;
-let missileX = 52.25;
+let invaders;
+let shipX = 47.75;
+let missileToShip = 95;
+let missileX = 50;
 let missileY = missileToShip;
 let tableInvaders = new Array();
-let invaderTop1 = "6%";
-let invaderTop2 = "12%";
-let invaderTop3 = "18%";
-let base = 10;
-
-for (let i = 0; i < 27; i++) {
-    tableInvaders[i] = new Array();
-}
+let invaderTop1 = "10%";
+let invaderTop2 = "20%";
+let invaderTop3 = "30%";
 
 //*** Enemies Generator ***//
 function invadersAreComing() {
     for (let i = 0; i < 27; i++) {
-    
+        tableInvaders[i] = new Array();
         let invader = document.createElement("div");
         invader.classList.add("invader");
 
@@ -47,28 +45,30 @@ function invadersAreComing() {
             tableInvaders[i].push(invaderTop3);
             tableInvaders[i].push(invader.style.left);
         }
-
+        invader.style.backgroundImage = `url(img/sprite_${Math.floor(Math.random() * 6)}.png)`;
     }
     console.log(tableInvaders);
+    invaders = document.querySelectorAll("div.invader");
 }
 
 //*** Is Enemy Here Verification ***//
-function isInvaderHere () {
-    for (let i = 0; i < tableInvaders.length; i++)
-    if (missileY == parse(tableInvaders[i][0]) && missileX == parse(tableInvaders[i][1])) {
-        console.log("touché")
+function isInvaderHere() {
+    for (let i = 0; i < tableInvaders.length; i++) {
+        if ((parseFloat(tableInvaders[i][0]) < missileY && missileY < parseFloat(tableInvaders[i][0]) + 2) && (parseFloat(tableInvaders[i][1]) < missileX && missileX < parseFloat(tableInvaders[i][1]) + 3)) {
+            invaders[i].classList.remove("invader");
+            tableInvaders[i][0] = null;
+            tableInvaders[i][1] = null;
+            score += 100;
+            showScore.textContent = `${score}`;
+        }
     }
-    console.log(missileY);
-    console.log(missileX);
-    console.log(parse(tableInvaders[i][0]))
 }
 
 //*** Missile Attack ***/
 function missileAttack() {
     if (missileY > 0) {
-        missileY = missileY - 0.5;
+        missileY = missileY - 2;
         theMissile.style.top = `${missileY}%`;
-        isInvaderHere();
         window.requestAnimationFrame(() => {
             missileAttack();
         })
@@ -77,6 +77,7 @@ function missileAttack() {
         missileY = missileToShip;
         theMissile.style.top = `${missileY}%`;
     }
+    isInvaderHere();
 }
 
 //*** Click Event Listener ***//
@@ -89,7 +90,7 @@ document.addEventListener("click", () => {
 document.addEventListener("keydown", (event) => {
     let key = event.code
     console.log(key)
-    if ((key === "KeyD" || key === "ArrowRight") && shipX < 95 && missileY == missileToShip) {
+    if ((key === "KeyD" || key === "ArrowRight") && shipX < 94.75 && missileY == missileToShip) {
         shipX += 1;
         theShip.style.left = `${shipX}%`;
         missileX += 1;
